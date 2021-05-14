8973960bcf6a20bbfd3d185feedaac81ecc91590
FirzenYogesh <contact@syogesh.dev>
2021-05-14 19:22:42 +0530

    null checks
    Signed-off-by: FirzenYogesh <contact@syogesh.dev>

---
+++ lib/pages/home_page_bottom_navigation_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomePageBottomNavigationBar extends StatefulWidget {
+  final List<BottomNavigationBarItem> items;
+  final BottomNavigationBarType bottomNavigationBarType;
+  final List<Widget> pages;
+  final AppBar appBar;
+
+  const HomePageBottomNavigationBar(
+      {Key key,
+      this.items,
+      this.pages,
+      this.bottomNavigationBarType,
+      this.appBar})
+      : super(key: key);
+  @override
+  _HomePageBottomNavigationBar createState() => _HomePageBottomNavigationBar();
+}
+
+class _HomePageBottomNavigationBar extends State<HomePageBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.items == null || widget.items.isEmpty) {
+      throw Exception("HomePageBottomNavigationBar: Missing items");
+    }
+    if (widget.pages != null && widget.items.length != widget.pages.length) {
+      throw Exception(
+          "HomePageBottomNavigationBar: Pages and Items count doesn't match");
+    }
+    return Scaffold(
+      appBar: widget.appBar,
+      body: SafeArea(
+          child: widget.pages != null
+              ? widget.pages[_selectedIndex]
+              : Center(child: Text("Page $_selectedIndex"))),
+      bottomNavigationBar: BottomNavigationBar(
+        items: widget.items,
+        type: widget.bottomNavigationBarType != null
+            ? widget.bottomNavigationBarType
+            : BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
