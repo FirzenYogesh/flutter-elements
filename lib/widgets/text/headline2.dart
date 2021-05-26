39fdbcf7c5084a0a29013424b3c506e3757540b0
FirzenYogesh <contact@syogesh.dev>
2021-05-26 13:46:06 +0530

    added text for easy access
    Signed-off-by: FirzenYogesh <contact@syogesh.dev>

---
+++ lib/widgets/text/headline2.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TextH2 extends StatelessWidget {
+  final String text;
+  final TextAlign textAlign;
+  final TextStyle textStyle;
+  final int maxLines;
+  final TextOverflow textOverflow;
+  final String semanticsLabel;
+  final bool softWrap;
+  final StrutStyle strutStyle;
+  final TextDirection textDirection;
+  final TextHeightBehavior textHeightBehavior;
+  final double textScaleFactor;
+  final TextWidthBasis textWidthBasis;
+
+  const TextH2(
+    this.text, {
+    Key key,
+    this.textAlign,
+    this.textStyle,
+    this.maxLines,
+    this.textOverflow,
+    this.semanticsLabel,
+    this.softWrap,
+    this.strutStyle,
+    this.textDirection,
+    this.textHeightBehavior,
+    this.textScaleFactor,
+    this.textWidthBasis,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Text(
+        text,
+        maxLines: maxLines,
+        overflow: textOverflow,
+        semanticsLabel: semanticsLabel,
+        softWrap: softWrap,
+        strutStyle: strutStyle,
+        textDirection: textDirection,
+        textHeightBehavior: textHeightBehavior,
+        textScaleFactor: textScaleFactor,
+        textWidthBasis: textWidthBasis,
+        textAlign: textAlign,
+        style: Theme.of(context).textTheme.headline2.merge(textStyle),
+      );
+}
