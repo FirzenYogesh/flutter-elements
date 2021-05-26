15ff8e05c512a3c46baf4623426771e2650baf3f
FirzenYogesh <contact@syogesh.dev>
2021-05-26 14:10:49 +0530

    exports
    Signed-off-by: FirzenYogesh <contact@syogesh.dev>

--- lib/elements.dart
+++ lib/elements.dart
@@ -1,17 +1,21 @@
 library elements;
 
-export 'package:elements/widgets/auto_size_text.dart';
-export 'package:elements/widgets/text/body_text1.dart';
-export 'package:elements/widgets/text/body_text2.dart';
-export 'package:elements/widgets/text/headline1.dart';
-export 'package:elements/widgets/text/headline1.dart';
-export 'package:elements/widgets/text/headline2.dart';
-export 'package:elements/widgets/text/headline3.dart';
-export 'package:elements/widgets/text/headline4.dart';
-export 'package:elements/widgets/text/headline5.dart';
-export 'package:elements/widgets/text/headline6.dart';
-export 'package:elements/widgets/text/subtitle1.dart';
-export 'package:elements/widgets/text/subtitle2.dart';
-export 'package:elements/widgets/text/caption.dart';
-export 'package:elements/widgets/text/button.dart';
-export 'package:elements/widgets/text/overline.dart';
+export 'widgets/auto_size_text.dart' show AutoSizeText;
+export 'widgets/text/body_text1.dart' show TextBody1;
+export 'widgets/text/body_text2.dart' show TextBody2;
+export 'widgets/text/headline1.dart' show TextH1;
+export 'widgets/text/headline2.dart' show TextH2;
+export 'widgets/text/headline3.dart' show TextH3;
+export 'widgets/text/headline4.dart' show TextH4;
+export 'widgets/text/headline5.dart' show TextH5;
+export 'widgets/text/headline6.dart' show TextH6;
+export 'widgets/text/subtitle1.dart' show TextSubtitle1;
+export 'widgets/text/subtitle2.dart' show TextSubtitle2;
+export 'widgets/text/caption.dart' show TextCaption;
+export 'widgets/text/button.dart' show TextB;
+export 'widgets/text/overline.dart' show TextOverline;
+export 'widgets/bottom_sheet.dart' show BottomSheetView;
+export 'widgets/flexible_divider.dart' show FlexibleDivider;
+export 'widgets/pages/bottom_navigation_bar.dart'
+    show HomePageBottomNavigationBar;
+export 'widgets/pages/single_page.dart' show SinglePageHome;
