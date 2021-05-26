4d037fc5893040e40cb11d65c794a32fb45749b8
FirzenYogesh <contact@syogesh.dev>
2021-05-26 14:05:17 +0530

    library mode
    Signed-off-by: FirzenYogesh <contact@syogesh.dev>

---
+++ lib/elements.dart
@@ -0,0 +1,17 @@
+library elements;
+
+export 'package:elements/widgets/auto_size_text.dart';
+export 'package:elements/widgets/text/body_text1.dart';
+export 'package:elements/widgets/text/body_text2.dart';
+export 'package:elements/widgets/text/headline1.dart';
+export 'package:elements/widgets/text/headline1.dart';
+export 'package:elements/widgets/text/headline2.dart';
+export 'package:elements/widgets/text/headline3.dart';
+export 'package:elements/widgets/text/headline4.dart';
+export 'package:elements/widgets/text/headline5.dart';
+export 'package:elements/widgets/text/headline6.dart';
+export 'package:elements/widgets/text/subtitle1.dart';
+export 'package:elements/widgets/text/subtitle2.dart';
+export 'package:elements/widgets/text/caption.dart';
+export 'package:elements/widgets/text/button.dart';
+export 'package:elements/widgets/text/overline.dart';
