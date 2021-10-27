4569654642bf5d955e1835e36006c0051d468a1f
FirzenYogesh <contact@syogesh.dev>
2021-10-27 13:50:09 +0530

    add device user agent

--- lib/elements_tools.dart
+++ lib/elements_tools.dart
@@ -1,5 +1,14 @@
 library elements_tools;
 
+import 'package:elements/src/tools/device.dart';
+
 export 'src/tools/api.dart' show ApiHelper, HTTP_METHOD;
+export 'src/tools/device.dart' show DeviceUtils;
 export 'src/tools/db.dart' show DB;
 export 'src/tools/utils.dart' show Utils, Environment;
+
+class Elements {
+  static void initialize() async {
+    await DeviceUtils.initialize();
+  }
+}
---
+++ lib/src/tools/device.dart
@@ -0,0 +1,20 @@
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceUtils {
+  static const _defaultUserAgent =
+      'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
+  late String _userAgent;
+
+  static late DeviceUtils instance;
+
+  DeviceUtils._();
+
+  static Future<void> initialize() async {
+    var deviceInfo = DeviceInfoPlugin();
+    DeviceUtils.instance = DeviceUtils._();
+    DeviceUtils.instance._userAgent =
+        (await deviceInfo.webBrowserInfo).userAgent ?? _defaultUserAgent;
+  }
+
+  String get userAgent => _userAgent;
+}
